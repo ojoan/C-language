89a6cbef2beb70e51caa7a293abc48ada57021e5
sedgee7 <nwedwols@gmail.com>
2026-01-14 22:56:11 -0300

    strings-conteudo

--- CONTEÚDOS/strings.cpp
+++ CONTEÚDOS/strings.cpp
@@ -1,3 +1,18 @@
-//
-// Created by joan on 13/01/2026.
-//
+#include <stdio.h>
+
+int main () {
+
+    char s[10];
+
+    printf("Digite algo (scanf convencional):\n");
+    scanf("%s", s);
+    fflush(stdin);
+
+    printf("Resultado: %s\n\n", s);
+
+    printf("Digite algo (scanf aprimorado):\n");
+    scanf("%10[^\n]s", s);
+    fflush(stdin);
+
+    printf("Resultado: %s\n\n", s);
+}
