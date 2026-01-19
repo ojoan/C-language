36b31064cdc28ad202b5f97c5d5293e23b83408b
sedgee7 <nwedwols@gmail.com>
2026-01-18 22:14:37 -0300

    atualização

--- CONTEÚDOS/strings.cpp
+++ CONTEÚDOS/strings.cpp
@@ -3,7 +3,7 @@
 int main () {
 
     char s[10];
-
+    /*
     printf("Digite algo (scanf convencional):\n");
     scanf("%s", s);
     fflush(stdin);
@@ -15,4 +15,20 @@ int main () {
     fflush(stdin);
 
     printf("Resultado: %s\n\n", s);
+    */
+
+    printf("Digite algo(leitura pelo GetS):\n");
+    gets(s);
+    fflush(stdin);
+
+    puts("Resultado:");
+    puts(s);
+    puts("");
+
+    printf("Digite algo(leitura pelo Fgets):\n");
+    fgets(s, 10, stdin);
+    fflush(stdin);
+
+    puts("Resultado:");
+    puts(s);
 }
