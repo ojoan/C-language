54882b2d6be0073daa8c4f0cd21718bed770ae2d
sedgee7 <nwedwols@gmail.com>
2026-01-25 21:34:59 -0300

    atualização

--- CONTEÚDOS/funcoes_procedimentos2.cpp
+++ CONTEÚDOS/funcoes_procedimentos2.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 void imprime1(int v[], int n) {
     int i;
     for (i = 0; i < n; i++) {
@@ -32,4 +32,23 @@ int main() {
 
     puts("\nTerceiro imprime:");
     imprime3(vet, 5);
+}
+*/
+void imprime(int m[][4], int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main () {
+    int mat[3][4] = {
+        {1,2,3,4},
+        {50,60,70,80},
+        {91,101,111,121}
+    };
+    imprime(mat, 3);
 }
