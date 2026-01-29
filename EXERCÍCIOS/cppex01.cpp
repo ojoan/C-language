a4f90258d7769d9c960df22736d245895476feab
sedgee7 <nwedwols@gmail.com>
2026-01-28 21:50:06 -0300

    Update cppex01.cpp

--- EXERCÍCIOS/cppex01.cpp
+++ EXERCÍCIOS/cppex01.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-
+    //Faça um programa que leia 4 números e faça a soma deles.
     int n1, n2, n3, n4, s;
-    printf("Informe quatro números em sequência:\n");
+    printf("Informe quatro numeros em sequencia:\n");
     scanf("%d", &n1);
     scanf("%d", &n2);
     scanf("%d", &n3);
@@ -11,6 +11,8 @@ int main() {
 
     s = n1 + n2 + n3 + n4;
 
-    printf("O resultado da soma entre os quatro números foi: %d.\n", s);
+    printf("O resultado da soma entre os quatro numeros foi: %d.\n", s);
+
+    return 0;
 
 }
