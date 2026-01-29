177f25b8d56457efd06465464d67d7ed1a97ebe8
sedgee7 <nwedwols@gmail.com>
2026-01-28 22:17:07 -0300

    atualização

---
+++ EXERCÍCIOS/cppex03.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/*Desenvolva um algoritmo que receba o tempo cronometrado em
+segundos, e digo quantas horas, minutos e segundos se passaram
+a partir do tempo cronometrado.*/
+
+int main() {
+    float seg, min, horas;
+    printf("Insira o tempo em segundos:\n");
+    scanf("%f", &seg);;
+
+    min = seg/60;
+    horas = (seg/60)/60;
+
+    printf("Horas: %2.f.\n", horas);
+    printf("minutos: %.2f.\n", min);
+    printf("segundos: %2.f.\n", seg);
+
+
+    return 0;
+}
