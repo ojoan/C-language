00c082323cad959b6fa237c209ee2874e9201483
sedgee7 <nwedwols@gmail.com>
2026-02-01 18:01:06 -0300

    exercicios

---
+++ EXERCÍCIOS/cppex08.cpp
@@ -0,0 +1,31 @@
+/*Desenvolva um algoritmo que mostre a
+tabuada de todos os números inteiros
+compreendidos entre 1 e 10(inclusive).
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int num, i, res;
+    /*
+    //Mostrar a tabuada do 1 até a 10
+    for (num = 1; num <= 10; num++) {
+        for (i = 1; i <= 10; i++) {
+            res = num * i;
+            printf("%d * %d = %d\n", num, i, res);
+        }
+    }
+   */
+    //Selecionar qual vai ser o número da tabuada
+    printf("Digite um numero:\n");
+    scanf("%d", &num);
+
+    for (i = 1; i <=10; i++) {
+        res = num * i;
+        printf("%d x %d = %d \n", num, i, res);
+    }
+
+    system("pause");
+    return 0;
+}
