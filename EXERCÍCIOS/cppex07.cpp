541624bdcc023060540dd1d3a62bf6ab501fab81
sedgee7 <nwedwols@gmail.com>
2026-02-01 14:33:12 -0300

    Create cppex07.cpp

---
+++ EXERCÍCIOS/cppex07.cpp
@@ -0,0 +1,17 @@
+/*Faça um programa que mostre os oito
+primeiros termos da sequência de Fibonacci.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int num1, num2, i;
+    num1 = 0;
+    num2 = 0;
+    i = 1;
+
+
+    system("pause");
+    return 0;
+}
