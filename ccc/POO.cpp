86b98b78e89d191e183ded0fcbe315ff4e215608
sedgee7 <nwedwols@gmail.com>
2026-03-17 21:32:15 -0300

    update

---
+++ ccc/POO.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Classe base
+class Pessoa {
+public:
+    string nome;
+    int idade;
+
+    void apresentar() {
+        cout << "Olá, meu nome é " << nome
+             << " e eu tenho " << idade << " anos." << endl;
+    }
+};
+
+// Classe derivada
+class Aluno : public Pessoa {
+public:
+    int matricula;
+
+    void estudar() {
+        cout << "O aluno esta estudando." << endl << endl;
+    }
+};
+
+// Classe derivada
+class Professor : public Pessoa {
+public:
+    string disciplina;
+
+    void ensinar() {
+        cout << "O professor " << nome
+             << " esta ensinando a disciplina de "
+             << disciplina << "." << endl;
+    }
+};
+
+int main() {
+
+    // aluno
+    Aluno aluno1;
+    aluno1.nome = "Carlos";
+    aluno1.idade = 12;
+
+    aluno1.apresentar();
+    aluno1.estudar();
+
+    // professor
+    Professor professor1;
+    professor1.nome = "Leonardo";
+    professor1.disciplina = "Linguagem Orientada a Objetos";
+
+    professor1.ensinar();
+
+    return 0;
+}
