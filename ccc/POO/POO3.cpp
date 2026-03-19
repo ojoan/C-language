01daffff841ac2afc5fd1e22b56b549072ecde44
sedgee7 <nwedwols@gmail.com>
2026-03-19 06:11:18 -0300

    update

---
+++ ccc/POO/POO3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+
+    cin>>number;
+
+    if (number > 0) {
+        cout<<"The number is positive: "<<number<<endl;
+    }
+    else if (number < 0) {
+        cout<<"The number is negative: "<<number<<endl;
+    }
+    else {
+        cout<<"The number is equal to zero: "<<number<<endl;
+    }
+
+    cout<<"End of the program";
+}
