e2c7966585c214e94ed016da3cdc804132a02387
sedgee7 <nwedwols@gmail.com>
2026-03-18 21:12:29 -0300

    update

--- ccc/POO/POO2.cpp
+++ ccc/POO/POO2.cpp
@@ -5,32 +5,32 @@ using namespace std;
 
 class Produto {
 private:
-    string nome;
-    double preco;
+    string name;
+    double price;
 
 public:
-    Produto(string nome, double preco){
-        this->nome = nome;
-        this->preco = preco;
+    Produto(string name, double price){
+        this->name = name;
+        this->price = price;
     }
 
-    Produto(string nome) {
-        this->nome = nome;
-        this->preco = 10;
+    Produto(string name) {
+        this->name = name;
+        this->price = 10;
     }
 
-    void exibirDetalhes() {
-        cout<<"Produto: "<< nome << ", Preco: "<<preco<<endl;
+    void showDetails() {
+        cout<<"Product: "<< name << ", Price: "<<price<<endl;
     }
 };
 
 int main() {
 
-    Produto p1("Caneta", 3.5);
-    Produto p2("Lapis");
+    Produto p1("Pen", 3.5);
+    Produto p2("pencil");
 
-    p1.exibirDetalhes();
-    p2.exibirDetalhes();
+    p1.showDetails();
+    p2.showDetails();
 
     return 0;
 }
